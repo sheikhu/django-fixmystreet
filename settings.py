570c13718123d2d9ad664bed6fa54421d8d0ce38
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2012-03-05 10:53:43 +0100

    fix settings when not in command line context

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, 'templates'),)
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
-if sys.argv[1] == 'test':
+if hasattr(sys, 'argv') and sys.argv[1] == 'test':
     tmp_dir = os.path.join(PROJECT_PATH, 'media-tmp')
     
     try:
