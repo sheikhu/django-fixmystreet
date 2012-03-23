8f6fc7ff08abdd944c798077fbe8991266e77d30
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-03-23 16:08:12 +0100

    first step eggs

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+version = "0.9 beta"
+
 setup(
     name = "django-fixmystreet",
-    version = "0.9 beta",
+    version = version,
     url = 'https://github.com/CIRB/django-fixmystreet',
     license = 'BSD',
     description = "",
     author = 'CIRB',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    install_requires = ['setuptools'],
+    packages=find_packages(exclude=['ez_setup']),
+    #package_dir = {'': 'src'},
+    install_requires = ['setuptools', 
+                        'django',],
     #extras_require = { 'test': ['django-debug-toolbar', 'django-jenkins'] }
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
 )
