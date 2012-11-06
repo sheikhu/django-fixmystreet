395a81ddcf366632a51dfffc093d8a8889f9aacc
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2012-11-06 09:25:18 +0100

    after install script

--- after-install.sh
+++ after-install.sh
@@ -1,4 +1,3 @@
-chown -R ibsa:ibsa /home/ibsa/Monitoring-Des-Quartiers
-sudo -u ibsa ln -fs /data/ibsa/attachments /home/ibsa/Monitoring-Des-Quartiers/src/static/attachments
-sudo -u ibsa /home/ibsa/Monitoring-Des-Quartiers/bin/django-deploy clear_cache
-#sudo -u ibsa /home/ibsa/Monitoring-Des-Quartiers/bin/django-deploy syncdb --migrate
+chown -R fixmystreet:fixmystreet /home/fixmystreet/django-fixmystreet
+sudo -u fixmystreet bin/django clear_cache
+sudo -u fixmystreet bin/django syncdb --migrate
