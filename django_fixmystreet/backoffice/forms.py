c8429ec00772455c981ebb799f0c87de55bda64e
lvanrymenant <lvanrymenant@csc.com>
2012-11-12 12:10:54 +0100

    Fix user not defined error for not logged in users

--- django_fixmystreet/backoffice/forms.py
+++ django_fixmystreet/backoffice/forms.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.utils.translation import ugettext_lazy
 from django.contrib.sessions.models import Session
+from django.contrib.auth.decorators import login_required
+
 
 class ManagersChoiceField (forms.fields.ChoiceField):
 	def __init__(self,  *args, **kwargs):
-		# assemble the opt groups.
 		choices = []
 		choices.append(('', ugettext_lazy("Select a manager")))
-		currentUserOrganisationId = FMSUser.objects.get(pk=getLoggedInUserId(Session.objects.all()[0].session_key)).organisation
+		currentUserOrganisationId = 1
+		if Session.objects.all()[0].session_key:
+			currentUserOrganisationId = FMSUser.objects.get(pk=getLoggedInUserId(Session.objects.all()[0].session_key)).organisation
 		managers = FMSUser.objects.filter(manager=True)
 		managers = managers.filter(organisation_id=currentUserOrganisationId)
 
@@ -26,6 +29,7 @@ class ManagersChoiceField (forms.fields.ChoiceField):
 		except FMSUser.DoesNotExist:
 			raise ValidationError(self.error_messages['invalid_choice'])
 		return model
+
 
 class ManagersListForm(forms.Form):
 	manager=ManagersChoiceField(label="")
