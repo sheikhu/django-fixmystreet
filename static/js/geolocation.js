610b8e380a42873f3b85e3e1fc46fbb4a27017f7
github@desany.eu <github@desany.eu>
2012-11-13 12:59:20 +0100

    add proj4js files

---
+++ static/js/geolocation.js
@@ -0,0 +1,16 @@
+//Create new object.
+UtilGeolocation = new Object();
+//Convertion geolocation EPSG instances
+UtilGeolocation.PROJ4JS_4326 = new Proj4js.Proj("EPSG:4326");
+UtilGeolocation.PROJ4JS_31370 = new Proj4js.Proj("EPSG:31370");                               
+
+/**
+ * convertCoordinatesToWMS is a method to convert GPS coordinates types to WMS ones.
+ * @param longitude: the longitude to convert
+ * @param latitude: the latitude to convert
+ */
+UtilGeolocation.convertCoordinatesToWMS = function(longitude, latitude) {
+     var p = new Proj4js.Point(longitude, latitude);
+     Proj4js.transform(UtilGeolocation.PROJ4JS_4326, UtilGeolocation.PROJ4JS_31370, p);
+     return p;
+}
