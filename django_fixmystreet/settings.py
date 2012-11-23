5165fbc386eafea4113d2373bacb091626e55a53
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2012-11-23 10:10:41 +0100

    clean settings and fix pyflakes

--- django_fixmystreet/settings.py
+++ django_fixmystreet/settings.py
@@ -7,7 +7,6 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 AUTH_PROFILE_MODULE = 'fixmystreet.UserProfile'
 LOGIN_REQUIRED_URLS = '^pro/'
 
-# TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, 'templates'),)
 if 'MEDIA_ROOT' in os.environ:
     MEDIA_ROOT = os.environ['MEDIA_ROOT']
 else:
@@ -17,33 +16,13 @@ STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
-#TEST_RUNNER = 'django.contrib.gis.tests.run_tests'
 POSTGIS_TEMPLATE = 'template_postgis'
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-# ADMIN_MEDIA_PREFIX = '/admin_media/'
-
-# ensure large uploaded files end up with correct permissions.  See
-# http://docs.djangoproject.com/en/dev/ref/settings/#file-upload-permissions
+TIME_ZONE = 'Europe/Brussels'
 
 FILE_UPLOAD_PERMISSIONS = 0644
 DATE_FORMAT = "l, j F Y"
 
-# List of callables that know how to import templates from various sources.
-# TEMPLATE_LOADERS = (
-    # 'django.template.loaders.filesystem.Loader',
-    # 'django.template.loaders.app_directories.Loader',
-    # 'django.template.loaders.eggs.load_template_source',
-# )
 
 # include request object in template to determine active page
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -53,7 +32,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.contrib.auth.context_processors.auth',
     "django.contrib.messages.context_processors.messages",
-    # 'social_auth.context_processors.social_auth_by_name_backends',
     'django_fixmystreet.fixmystreet.utils.domain_context_processor'
 )
 
@@ -66,12 +44,7 @@ MIDDLEWARE_CLASSES = (
     'django_fixmystreet.backoffice.middleware.LoginRequiredMiddleware'
 )
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = os.environ['LANGUAGE_CODE'] if 'LANGUAGE_CODE' in os.environ else 'fr'
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
 
 gettext = lambda s: s
@@ -93,7 +66,6 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     
     'transmeta',
-    # 'social_auth',
     'south',
     'django_extensions',
     'django_fixmystreet.fixmystreet',
@@ -101,49 +73,50 @@ INSTALLED_APPS = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    # 'social_auth.backends.twitter.TwitterBackend',
-    # 'social_auth.backends.facebook.FacebookBackend',
-    # 'social_auth.backends.google.GoogleOAuthBackend',
-    # 'social_auth.backends.google.GoogleOAuth2Backend',
-    # 'social_auth.backends.google.GoogleBackend',
-    # 'social_auth.backends.yahoo.YahooBackend',
-    # 'social_auth.backends.contrib.linkedin.LinkedinBackend',
-    # 'social_auth.backends.contrib.livejournal.LiveJournalBackend',
-    # 'social_auth.backends.contrib.orkut.OrkutBackend',
-    # 'social_auth.backends.contrib.foursquare.FoursquareBackend',
-    # 'social_auth.backends.contrib.github.GithubBackend',
-    # 'social_auth.backends.OpenIDBackend',
     'django.contrib.auth.backends.ModelBackend',
-    # 'django_fixmystreet.fixmystreet.googlebackend.GoogleProfileBackend',
 )
 
-# SOCIAL_AUTH_ENABLED_BACKENDS = ('google-profile', 'facebook')
-
-# SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/login-callback/'
-
-FACEBOOK_EXTENDED_PERMISSIONS = ('email',)
-
-#for client secret see local settings
-FACEBOOK_APP_ID             = '263584440367959'
-GOOGLE_OAUTH2_CLIENT_ID     = '985105651100.apps.googleusercontent.com'
-
-
-LOGIN_REDIRECT_URL = '/'
-LOGIN_ERROR_URL    = '/'
-
-
-ADD_THIS_KEY = "broken" #"xa-4a620b09451f9502"
+ADD_THIS_KEY = os.environ['ADD_THIS_KEY'] if 'ADD_THIS_KEY' in os.environ else None
 
 EMAIL_FROM_USER = "Fix My Street<fixmystreet@cirb.irisnet.be>"
 DEFAULT_FROM_EMAIL = "Fix My Street<fixmystreet@cirb.irisnset.be>"
 
 
+# supported value of ENVIRONMENT are dev, jenkins, staging, production
 if "ENV" in os.environ:
-    # supported value of ENVIRONMENT are dev, jenkins, staging, production
     ENVIRONMENT = os.environ['ENV']
 else:
     ENVIRONMENT = "local"
     sys.stderr.write( "No ENV specified, using local.\n" ) 
+
+
+
+try:
+    __import__('gunicorn')
+    print "using gunicorn"
+    INSTALLED_APPS += ('gunicorn', )
+except ImportError:
+    pass
+
+try:
+    __import__('debug_toolbar')
+    print "using debug toolbar"
+    INSTALLED_APPS += ('debug_toolbar', )
+    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INTERNAL_IPS = ('127.0.0.1',)
+except ImportError:
+    pass
+
+try:
+    __import__('django_jenkins')
+    INSTALLED_APPS += ('django_jenkins',)
+    PROJECT_APPS = ('fixmystreet',)
+    JENKINS_TASKS = (
+        'django_jenkins.tasks.with_coverage',
+        'django_jenkins.tasks.django_tests',
+    )
+except ImportError:
+    pass
 
 
 
@@ -156,45 +129,13 @@ else:
     GEOSERVER = "geoserver.gis.irisnet.be"
     SERVICE_GIS = "service.gis.irisnet.be"
 
-try:
-    import gunicorn
-    print "using gunicorn"
-    INSTALLED_APPS += ('gunicorn', )
-except ImportError:
-    pass
-
-
-
-try:
-    import debug_toolbar
-    print "using debug toolbar"
-    INSTALLED_APPS += ('debug_toolbar', )
-    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-    INTERNAL_IPS = ('127.0.0.1',)
-except ImportError:
-    pass
-
 if ENVIRONMENT=="local":
     logging.basicConfig(
         level = logging.DEBUG,
         format = '%(asctime)s %(levelname)s %(message)s',
-        filename = './fixmystreet.log',
+        filename = os.path.join(PROJECT_PATH, 'fixmystreet.log'),
         filemode = 'w'
     )
-
-try:
-    import django_jenkins
-    INSTALLED_APPS += ('django_jenkins',)
-    PROJECT_APPS = ('fixmystreet',)
-    JENKINS_TASKS = (
-        # 'django_jenkins.tasks.run_pylint',
-        'django_jenkins.tasks.with_coverage',
-        'django_jenkins.tasks.django_tests',
-        #'django_jenkins.tasks.run_jslint',
-    )
-except ImportError:
-    pass
-
 
 if ENVIRONMENT=="dev" or ENVIRONMENT=="local" or ENVIRONMENT=="jenkins":
     SITE_ID = 3
@@ -206,11 +147,6 @@ elif ENVIRONMENT=="staging":
 elif ENVIRONMENT=="production":
     SITE_ID = 1
     SITE_URL = "fixmystreet.irisnet.be"
-
-
-JSLINT_CHECKED_FILES = (
-    'media/js/fixmystreetmap.js'
-)
 
 
 try:
