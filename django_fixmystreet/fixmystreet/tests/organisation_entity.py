fee538b78392d6d3feb5c01f36b33841c702544a
github@desany.eu <github@desany.eu>
2012-11-26 11:43:21 +0100

    a unit tests on organisations

--- django_fixmystreet/fixmystreet/tests/organisation_entity.py
+++ django_fixmystreet/fixmystreet/tests/organisation_entity.py
@@ -16,67 +16,50 @@ class OrganisationEntityTest(TestCase):
     fixtures = ["bootstrap","list_items"]
     
     def setUp(self):
-       self.organisation = OrganisationEntity(name="Dummy Organisation", commune=False, region=False)
+       '''Create organisation test structure'''
+       self.organisation_dependency = OrganisationEntity(name="Dependency Organisation", commune=False, region=True, subcontractor=False, applicant=False)
+       self.organisation_dependency.save()
+       self.organisation = OrganisationEntity(name="Dummy Organisation", commune=False, region=False, subcontractor=False, applicant=False, dependency=self.organisation_dependency, feature_id = 5)
        self.organisation.save()
 
-       # these are from the fixtures file.
-       self.category = ReportCategory.objects.all()[0]
-       self.categoryclass = self.category.category_class
-
-       self.commune = OrganisationEntity(name='test ward')
-       #Create a FMSUser
-       self.fmsuser = FMSUser(telephone="0123456789", last_used_language="fr", agent=False, manager=False, leader=False, impetrant=False, contractor=False)
-       self.fmsuser.save();
-
-    def testCreationOfFMSUser(self):
-       '''Create a user and check if the row in database has been created'''
-       self.assertTrue(self.fmsuser.id > 0)
+    def testCreationOfOrganisation(self):
+       '''Create an organisation and check if the row in database has been created'''
+       self.assertTrue(self.organisation.id > 0)
     
-    def testFMSCitizenOrProRole(self):
+    def testOrganisationRoles(self):
        '''Test the roles of the FMSUser created'''
-       self.assertTrue(self.fmsuser.is_citizen())
-       self.assertFalse(self.fmsuser.is_pro())
-       self.fmsuser.agent = True
-       self.assertFalse(self.fmsuser.is_citizen())
-       self.assertTrue(self.fmsuser.is_pro())
-       self.fmsuser.agent = False
-       self.fmsuser.manager = True 
-       self.assertFalse(self.fmsuser.is_citizen())
-       self.assertTrue(self.fmsuser.is_pro())
-       self.fmsuser.manager = False
-       self.fmsuser.leader = True 
-       self.assertFalse(self.fmsuser.is_citizen())
-       self.assertTrue(self.fmsuser.is_pro())
-       self.fmsuser.leader = False
-       self.fmsuser.impetrant = True 
-       self.assertFalse(self.fmsuser.is_citizen())
-       self.assertTrue(self.fmsuser.is_pro())
-       self.fmsuser.impetrant = False
-       self.fmsuser.contractor = True 
-       self.assertFalse(self.fmsuser.is_citizen())
-       self.assertTrue(self.fmsuser.is_pro())
-       self.fmsuser.contractor = False
-       self.assertTrue(self.fmsuser.is_citizen())
-       self.assertFalse(self.fmsuser.is_pro())
+       self.assertFalse(self.organisation.is_commune())
+       self.assertFalse(self.organisation.is_region())
+       self.assertFalse(self.organisation.is_subcontractor())
+       self.assertFalse(self.organisation.is_applicant())
+       
+       self.organisation.commune=True
+       self.assertTrue(self.organisation.is_commune())
+       self.assertFalse(self.organisation.is_region())
+       self.assertFalse(self.organisation.is_subcontractor())
+       self.assertFalse(self.organisation.is_applicant())
+       
+       self.organisation.commune=False
+       self.organisation.region=True
+       self.assertFalse(self.organisation.is_commune())
+       self.assertTrue(self.organisation.is_region())
+       self.assertFalse(self.organisation.is_subcontractor())
+       self.assertFalse(self.organisation.is_applicant())
+       
+       self.organisation.region=False
+       self.organisation.subcontractor=True
+       self.assertFalse(self.organisation.is_commune())
+       self.assertFalse(self.organisation.is_region())
+       self.assertTrue(self.organisation.is_subcontractor())
+       self.assertFalse(self.organisation.is_applicant())
+       
+       self.organisation.subcontractor=False
+       self.organisation.applicant=True
+       self.assertFalse(self.organisation.is_commune())
+       self.assertFalse(self.organisation.is_region())
+       self.assertFalse(self.organisation.is_subcontractor())
+       self.assertTrue(self.organisation.is_applicant())
     
-    def testFMSLanguage(self):
-       '''Test the user language'''
-       self.assertEquals(self.fmsuser.last_used_language, "fr")
-    
-    def testFMSSpecificRoles(self):
-       '''Test the user roles boolean values'''
-       self.assertFalse(self.fmsuser.agent)
-       self.assertFalse(self.fmsuser.manager)
-       self.assertFalse(self.fmsuser.leader)
-       self.assertFalse(self.fmsuser.impetrant)
-       self.assertFalse(self.fmsuser.contractor)
-       self.fmsuser.agent = True 
-       self.fmsuser.manager = True 
-       self.fmsuser.leader = True 
-       self.fmsuser.impetrant = True 
-       self.fmsuser.contractor = True 
-       self.assertTrue(self.fmsuser.agent)
-       self.assertTrue(self.fmsuser.manager)
-       self.assertTrue(self.fmsuser.leader)
-       self.assertTrue(self.fmsuser.impetrant)
-       self.assertTrue(self.fmsuser.contractor)
+    def testOrganisationDependency(self):
+       '''Test the organisation dependency'''
+       self.assertTrue(self.organisation_dependency.id > 0)
