dd145fcb709bc391b7a95ad487a540191820d242
github@desany.eu <github@desany.eu>
2013-01-04 12:32:12 +0100

    add the map to pdfs

--- django_fixmystreet/backoffice/views/reports/updates.py
+++ django_fixmystreet/backoffice/views/reports/updates.py
@@ -138,13 +138,18 @@ def changeContractor(request,report_id):
 def reportPdf(request, report_id, pro_version):
     '''reportPdf is called from report details page to generate the pdf with report story. When pro_version == 0 then filter pdf content'''
     report = get_object_or_404(Report, id=report_id)
-
+    
     #Verify if the connected user is well pro ! (Server side protection)
     # if request.user.fmsuser.is_citizen():
        # pro_Version = 0
-
-    return render_to_pdf(request, "pro/pdf.html", {'report' : report, 'file_list' : report.files.all(), 'comment_list' : report.comments.all(), 'activity_list' : report.activities.all(), 'pro_version' : pro_version},
+    
+    if request.GET.get('output', False):
+        return render_to_response("pro/pdf.html", {'report' : report, 'file_list' : report.files.all(), 'comment_list' : report.comments.all(), 'activity_list' : report.activities.all() ,'pro_version': pro_version},
                 context_instance=RequestContext(request))
+    else:
+        return render_to_pdf("pro/pdf.html", {'report' : report, 'file_list' : report.files.all(), 'comment_list' : report.comments.all(), 'activity_list' : report.activities.all(), 'pro_version' : pro_version},
+                context_instance=RequestContext(request))
+
 
 def acceptAndValidate(request, report_id):
     report = get_object_or_404(Report, id=report_id)
@@ -184,20 +189,7 @@ def updateComment(request,report_id):
     report = get_object_or_404(Report,id=report_id)
     security_level = request.REQUEST.get('updateType')
     comment = ReportComment.objects.get(pk=request.REQUEST.get('commentId'))
-<<<<<<< HEAD
     comment.security_level = comment.get_security_level(int(security_level))
-
-=======
-    if updateType == "valid":
-        comment.is_validated = (request.REQUEST.get('updateValue')=='checked')
-        if comment.is_validated:
-            comment.is_visible= True
-    if updateType == 'confidential':
-        comment.is_visible = not (request.REQUEST.get('updateValue')=='checked')
-        if comment.is_visible == False:
-            comment.is_validated = False #When setting element to confidential then it becomes unvalidated automatically
-
->>>>>>> fix pdf export
     comment.save()
     if "pro" in request.path:
             return HttpResponseRedirect(report.get_absolute_url_pro())
--- django_fixmystreet/fixmystreet/views/reports/updates.py
+++ django_fixmystreet/fixmystreet/views/reports/updates.py
@@ -43,11 +43,15 @@ def new( request, report_id ):
 def reportPdf(request, report_id, pro_version):
     '''reportPdf is called from report details page to generate the pdf with report story. When pro_version == 0 then filter pdf content'''
     report = get_object_or_404(Report, id=report_id)
-
+    
     #Set pro version to 0 per default as this view method should always be called by the citizen version of the webapp
     # pro_Version = 0
 
-
-    return render_to_pdf(request, "pro/pdf.html", {'report' : report, 'pro_version' : pro_version},
+    if request.GET.get('output', False):
+        return render_to_response("pro/pdf.html", {
+            'report' : report,
+            'pro_version' : pro_version
+        }, context_instance=RequestContext(request))
+    else:
+        return render_to_pdf("pro/pdf.html", {'report' : report, 'pro_version' : pro_version},
                 context_instance=RequestContext(request))
-
