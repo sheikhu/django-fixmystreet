9c0bc596d9b699b304be3972f37dc26942c6c762
github@desany.eu <github@desany.eu>
2013-01-30 15:32:55 +0100

    merge css

--- django_fixmystreet/fixmystreet/models.py
+++ django_fixmystreet/fixmystreet/models.py
@@ -398,6 +398,9 @@ class Report(UserTrackedModel):
 
     def get_address_city_name(self):
         return ZipCode.objects.get(code=self.postalcode).name
+    
+    def get_number_of_subscription(self):
+        return self.subscriptions.all().__len__()
 
     def display_category(self):
         return self.category.name + " / " + self.secondary_category.secondary_category_class.name + " : " + self.secondary_category.name
