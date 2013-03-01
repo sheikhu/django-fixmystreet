0520ff116c9fc7275883e13a1a8a5f9eb8a6cc4a
CIRB Jenkins <jenkins@cirb.irisnet.be>
2013-03-01 10:05:11 +0100

    Preparing release 0.3.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog of django-fixmystreet
 ===================================================
 
 
-0.3.2 (unreleased)
+0.3.2 (2013-03-01)
 ------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import os
 
-version = '0.3.2.dev0'
+version = '0.3.2'
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 long_description = '\n\n'.join([
