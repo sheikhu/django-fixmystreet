d0c83af14d927741f7a770ad7d3dcd12f08818b2
L'Ecluse Filip <flecluse@Mac-mini-CIRB.local>
2013-10-24 11:26:14 +0200

    FMS-42 added new screen to create a report based from the map

--- django_fixmystreet/fixmystreet/views/reports/main.py
+++ django_fixmystreet/fixmystreet/views/reports/main.py
@@ -225,3 +225,14 @@ def index(request, slug=None, commune_id=None):
     return render_to_response("reports/index.html", {
         "communes": communes
     }, context_instance=RequestContext(request))
+
+def newmap(request):
+    pro_user = False
+    try:
+        pro_user = request.fmsuser.manager
+    except  AttributeError:
+        pro_user = False    
+    return render_to_response("reports/newmap.html", { 
+        "all_zips":ZipCode.objects.all(),
+        "pro_user":pro_user,
+        }, context_instance=RequestContext(request))
