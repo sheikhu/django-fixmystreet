f3f94efcf4ed4cf3f1f026073acd94b65ac9d70a
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2013-12-16 17:06:16 +0100

    translations...

--- django_fixmystreet/backoffice/urls.py
+++ django_fixmystreet/backoffice/urls.py
@@ -8,35 +8,39 @@ from django_fixmystreet.fixmystreet.api import ReportHandler
 # from django_fixmystreet.backoffice.views.users import CreateUser
 
 
-urlpatterns = patterns('django_fixmystreet.backoffice.views.main',
-    url(_(r'^$'), 'home',name='home_pro'),
+urlpatterns = patterns(
+    'django_fixmystreet.backoffice.views.main',
+    url(_(r'^$'), 'home', name='home_pro'),
 )
 
-urlpatterns += patterns('django_fixmystreet.backoffice.views.reports.main',
-    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)$'), 'show',name='report_show_pro'),
-    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/document$'), 'document',name='report_document_pro'),
-    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/merge$'), 'merge',name='report_merge_pro'),
-    url(_(r'^report/new$'), 'new',name='report_new_pro'),
-    url(_(r'^report/verify'), 'verify',name='report_verify_pro'),
-    url(_(r'^report/subscription$'), 'subscription',name='report_subscription_pro'),
-    url(_(r'^report/search$'), 'search_ticket_pro',name='search_ticket_pro'),
-    url(_(r'^report/$'), 'report_prepare_pro',name='report_prepare_pro'),
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.reports.main',
+    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)$'), 'show', name='report_show_pro'),
+    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/document$'), 'document', name='report_document_pro'),
+    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/merge$'), 'merge', name='report_merge_pro'),
+    url(_(r'^report/new$'), 'new', name='report_new_pro'),
+    url(_(r'^report/verify'), 'verify', name='report_verify_pro'),
+    url(_(r'^report/subscription$'), 'subscription', name='report_subscription_pro'),
+    url(_(r'^report/search$'), 'search_ticket_pro', name='search_ticket_pro'),
+    url(_(r'^report/$'), 'report_prepare_pro', name='report_prepare_pro'),
     url(_(r'^report/(\d+)/delete/$'), 'delete', name='report_delete_pro'),
 )
 
-urlpatterns += patterns('django_fixmystreet.backoffice.views.reports.list',
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.reports.list',
     url(_(r'^report/table/$'), 'all_reports', name='report_table_pro'),
     url(_(r'^report/table-content/$'), 'table_content', name='report_table_content_pro'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.backoffice.views.reports.updates',
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.reports.updates',
     url(_(r'^report/(\d+)/accept/$'), 'accept', name='report_accept_pro'),
     url(_(r'^report/(\d+)/refuse/$'), 'refuse', name='report_refuse_pro'),
     url(_(r'^report/(\d+)/fixed/$'), 'fixed', name='report_fix_pro'),
     url(_(r'^report/(\d+)/close/$'), 'close', name='report_close_pro'),
-    url(_(r'^report/(\d+)/merge/$'),'do_merge',name='report_do_merge_pro'),
+    url(_(r'^report/(\d+)/merge/$'), 'do_merge', name='report_do_merge_pro'),
     url(_(r'^report/(\d+)/planned/$'), 'planned', name='report_planned_pro'),
-    url(_(r'^report/(\d+)/validateAll/$'),'validateAll',name='report_validate_all_pro'),
+    url(_(r'^report/(\d+)/validateAll/$'), 'validateAll', name='report_validate_all_pro'),
     url(_(r'^report/(\d+)/updateAttachment$'), 'updateAttachment', name='report_update_attachment'),
     url(_(r'^report/(\d+)/deleteFile$'), 'deleteAttachment', name='report_delete_attachment'),
     url(_(r'^report/(\d+)/changeManager/'), 'changeManager', name='report_change_manager_pro'),
@@ -44,41 +48,41 @@ urlpatterns += patterns( 'django_fixmystreet.backoffice.views.reports.updates',
     url(_(r'^report/(\d+)/switchPrivacy/'), 'switchPrivacy', name='report_change_switch_privacy'),
     url(_(r'^report/(\d+)/publish/'), 'publish', name='report_publish_pro'),
 )
-urlpatterns += patterns( 'django_fixmystreet.fixmystreet.views.reports.updates',
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.reports.updates',
     url(_(r'^report/(\d+)/pdf/'), 'report_pdf', {"pro_version": True}, name='report_pdf_pro'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.backoffice.views.reports.subscribers',
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.reports.subscribers',
     url(_(r'^report/(\d+)/subscribe/'), 'create', name='subscribe_pro'),
-    url(_(r'^report/(\d+)/unsubscribe/'), 'unsubscribe',name='unsubscribe_pro'),
+    url(_(r'^report/(\d+)/unsubscribe/'), 'unsubscribe', name='unsubscribe_pro'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.backoffice.views.reports.flags',
-    url(_(r'^report/(\d+)/flags/thanks'), 'thanks',name='flag_success_pro'),
-    url(_(r'^report/(\d+)/flags'), 'new',name='flag_report_pro'),
-)
-
-urlpatterns += patterns('django_fixmystreet.backoffice.views.manager_category_configuration',
-    url(r'^category-gestionnaire-configuration/dialog/','update',name='gestionnaire_selection_dialog'),
-    url(r'^category-gestionnaire-configuration','show',name='category_gestionnaire_configuration'),
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.manager_category_configuration',
+    url(r'^category-gestionnaire-configuration/dialog/', 'update', name='gestionnaire_selection_dialog'),
+    url(r'^category-gestionnaire-configuration', 'show', name='category_gestionnaire_configuration'),
 
 )
 
-urlpatterns += patterns('django_fixmystreet.backoffice.views.ajax',
-    url(_(r'^ajax/saveSelection'),'saveCategoryConfiguration',name='saveSelection'),
-    url(_(r'^ajax/reportPopupDetails'),'get_report_popup_details',name="get_report_popup_details_pro"),
-    url(_(r'^ajax/report/(\d+)/updatePriority/'),'updatePriority',name="report_update_priority"),
-    url(_(r'^ajax/map/filter/'),'filter_map',name="filter_map"),
-    url(_(r'^ajax/report/(\d+)/false_address/'),'report_false_address',name="report_false_address"),
-    url(_(r'^ajax/secondary_category_for_main_category'),'secondary_category_for_main_category',name="secondary_category_for_main_category"),
-    url(_(r'^ajax/update_category_for_report/(\d+)'),'update_category_for_report',name="update_category_for_report"),
-    url(_(r'^ajax/send_pdf/(\d+)'),'send_pdf',name="send_pdf"),
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.ajax',
+    url(r'^ajax/saveSelection', 'saveCategoryConfiguration', name='saveSelection'),
+    url(r'^ajax/reportPopupDetails', 'get_report_popup_details', name="get_report_popup_details_pro"),
+    url(r'^ajax/report/(\d+)/updatePriority/', 'updatePriority', name="report_update_priority"),
+    url(r'^ajax/map/filter/', 'filter_map', name="filter_map"),
+    url(r'^ajax/report/(\d+)/false_address/', 'report_false_address', name="report_false_address"),
+    url(r'^ajax/secondary_category_for_main_category', 'secondary_category_for_main_category', name="secondary_category_for_main_category"),
+    url(r'^ajax/update_category_for_report/(\d+)', 'update_category_for_report', name="update_category_for_report"),
+    url(r'^ajax/send_pdf/(\d+)', 'send_pdf', name="send_pdf"),
 )
 
-urlpatterns += patterns('django_fixmystreet.backoffice.views.users',
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.users',
     # (r'^accounts/', include('registration.backends.simple.urls')),
-    url(_(r'^logout/$'),'logout_view', name='logout'),
-    url(_(r'^change-password/$'), 'change_password', name='password_change'),
+    url(r'^logout/$', 'logout_view', name='logout'),
+    url(r'^change-password/$', 'change_password', name='password_change'),
     #url(_(r'^change_password/$'), 'django.contrib.auth.views.password_change', {'template_name': 'pro/change_password.html','post_change_redirect':'/pro/'}, name='password_change'),
 
     url(r'^users/$', 'list_users', name='list_users'),
@@ -91,9 +95,10 @@ urlpatterns += patterns('django_fixmystreet.backoffice.views.users',
     # url(r'^contractors/(?P<contractor_id>\d+)/delete$', 'delete_contractor', name='delete_contractor'),
 )
 
-urlpatterns +=patterns('django_fixmystreet.backoffice.views.groups',
-    url(r'^groups/$',                   'list_groups',  name='list_groups'),
-    url(r'^groups/create/$',            'create_group', name='create_group'),
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.groups',
+    url(r'^groups/$',        'list_groups',  name='list_groups'),
+    url(r'^groups/create/$', 'create_group', name='create_group'),
 
     url(r'^groups/(?P<group_id>\d+)/$', 'edit_group',   name='edit_group'),
     url(r'^groups/(?P<group_id>\d+)/delete/$', 'delete_group',   name='delete_group'),
@@ -103,7 +108,8 @@ urlpatterns +=patterns('django_fixmystreet.backoffice.views.groups',
     url(r'^groups/membership/contact/(?P<membership_id>\d+)/$', 'contact_membership',   name='contact_membership'),
 )
 
-urlpatterns += patterns('',
+urlpatterns += patterns(
+    '',
     url(_(r'^export_file/reports/(?P<emitter_format>.+)/(?P<id>\d+)'), Resource(ReportHandler), name="export_report"),
     url(_(r'^export_file/reports/(?P<emitter_format>.+)/fromtime/(?P<time_ago>.+)'), Resource(ReportHandler), name="export_report"),
     url(_(r'^export_file/reports/(?P<emitter_format>.+)'), Resource(ReportHandler), name="export_report"),
--- django_fixmystreet/fixmystreet/urls.py
+++ django_fixmystreet/fixmystreet/urls.py
@@ -6,110 +6,85 @@ from piston.resource import Resource
 from django_fixmystreet.fixmystreet.api import ReportHandler
 #from django_fixmystreet.fixmystreet.views.api import CitizenReportHandler, ProReportHandler
 
-urlpatterns = patterns('django_fixmystreet.fixmystreet.views.main',
-    url(_(r'^$'), 'home',name='home'),
-    url(_(r'^about/$'), 'about',name='about'),
-    url(_(r'^posters/$'), 'posters',name='posters'),
-    url(_(r'^terms-of-use/$'), 'terms_of_use',name='terms_of_use'),
-    url(_(r'^update-language/'),'update_current_language',name="update_current_language"),
+urlpatterns = patterns(
+    'django_fixmystreet.fixmystreet.views.main',
+    url(_(r'^$'), 'home', name='home'),
+    url(_(r'^about/$'), 'about', name='about'),
+    url(_(r'^terms-of-use/$'), 'terms_of_use', name='terms_of_use'),
+    url(r'^update-language/', 'update_current_language', name="update_current_language"),
 )
 
 
-urlpatterns += patterns('django_fixmystreet.backoffice.views.users',
-    url(_(r'^login/$'), 'login_view', name='login'),
+urlpatterns += patterns(
+    'django_fixmystreet.backoffice.views.users',
+    url(r'^login/$', 'login_view', name='login'),
 )
 
-urlpatterns += patterns('django.contrib.auth.views',
-    (r'^accounts/password/reset/$', 'password_reset',
-            {'post_reset_redirect' : '/accounts/password/reset/done/', 'template_name': 'admin/registration/password_reset_form.html'}),
+urlpatterns += patterns(
+    'django.contrib.auth.views',
+    (r'^accounts/password/reset/$', 'password_reset', {
+        'post_reset_redirect': '/accounts/password/reset/done/',
+        'template_name': 'admin/registration/password_reset_form.html'
+    }),
     (r'^accounts/password/reset/done/$', 'password_reset_done', {'template_name': 'admin/registration/password_reset_done.html'}),
-    (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm',
-            {'post_reset_redirect' : '/accounts/password/done/', 'template_name': 'admin/registration/password_reset_confirm.html'}),
+    (r'^accounts/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {
+        'post_reset_redirect': '/accounts/password/done/',
+        'template_name': 'admin/registration/password_reset_confirm.html'
+    }),
     (r'^accounts/password/done/$', 'password_reset_complete', {'template_name': 'admin/registration/password_reset_complete.html'}),
 )
 
-urlpatterns += patterns('django_fixmystreet.fixmystreet.views.promotion',
-    (_(r'^promotions/(\w+)$'), 'show'),
-)
-
-# urlpatterns += patterns('django_fixmystreet.fixmystreet.views.wards',
-    # (r'^communes/$', cities.show, {"city_id":1}, 'bxl_wards'),
-    # url(r'^commune/(\d+)', 'show',name='ward_show'),
-# )
-
-#urlpatterns += patterns('django_fixmystreet.fixmystreet.views.cities',
-#    url(r'^city/(\d+)', 'show',name='city_show'),
-#)
-
-urlpatterns += patterns('django_fixmystreet.fixmystreet.views.reports.main',
-    url(_(r'^reports/$'), 'index',name='report_index'),
-    url(_(r'^reports/(?P<slug>.*)/(?P<commune_id>\d+)$'), 'index',name='report_commune_index'),
-    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)$'), 'show',name='report_show'),
-    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/document$'), 'document',name='report_document'),
-    url(_(r'^report/search'), 'search_ticket',name='search_ticket'),
-    url(_(r'^report/new'), 'new',name='report_new'),
-    url(_(r'^report/verify'), 'verify',name='report_verify'),
-    url(_(r'^report/prepare'), 'report_prepare',name='report_prepare'),
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.reports.main',
+    url(_(r'^reports/$'), 'index', name='report_index'),
+    url(_(r'^reports/(?P<slug>.*)/(?P<commune_id>\d+)$'), 'index', name='report_commune_index'),
+    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)$'), 'show', name='report_show'),
+    url(_(r'^report/(?P<slug>.*)/(?P<report_id>\d+)/document$'), 'document', name='report_document'),
+    url(_(r'^report/search'), 'search_ticket', name='search_ticket'),
+    url(_(r'^report/new'), 'new', name='report_new'),
+    url(_(r'^report/verify'), 'verify', name='report_verify'),
+    url(_(r'^report/prepare'), 'report_prepare', name='report_prepare'),
     url(_(r'^report/(\d+)/update/'), 'update', name='report_update'),
     url(_(r'^report/newmap/'), 'newmap', name='report_new_map'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.fixmystreet.views.reports.updates',
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.reports.updates',
     url(_(r'^report/(\d+)/pdf/(pro)?'), 'report_pdf', name='report_pdf'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.fixmystreet.views.reports.subscribers',
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.reports.subscribers',
     url(_(r'^report/(\d+)/subscribe/'), 'create', name='subscribe'),
     url(_(r'^report/(\d+)/unsubscribe/'), 'remove', name='unsubscribe'),
 )
 
-urlpatterns += patterns( 'django_fixmystreet.fixmystreet.views.reports.flags',
-    url(_(r'^report/(\d+)/flags/thanks'), 'thanks',name='flag_success'),
-    url(_(r'^report/(\d+)/flags'), 'new',name='flag_report'),
-)
-
-# urlpatterns += patterns('django_fixmystreet.fixmystreet.views.contact',
-#     (_(r'^contact/thanks'), 'thanks'),
-#     (_(r'^contact/'), 'new', {}, 'contact'),
-# )
-
-urlpatterns += patterns('django_fixmystreet.fixmystreet.views.ajax',
-
-    url(_(r'^ajax/create-comment'),'create_comment',name='create_report_comment'),
-    url(_(r'^ajax/create-file'),'create_file',name='create_report_file'),
-    url(_(r'^ajax/categories/(\d+)'), 'report_category_note',name='report_category_note'),
-    url(_(r'^ajax/upload-file'),'uploadFile',name='report_upload_file'),
-    url(_(r'^ajax/reportPopupDetails'),'get_report_popup_details',name="get_report_popup_details"),
-    url(_(r'^ajax/map/filter/'),'filter_map',name="filter_map"),
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.ajax',
+    url(r'^ajax/create-comment', 'create_comment', name='create_report_comment'),
+    url(r'^ajax/create-file', 'create_file', name='create_report_file'),
+    url(r'^ajax/categories/(\d+)', 'report_category_note', name='report_category_note'),
+    url(r'^ajax/upload-file', 'uploadFile', name='report_upload_file'),
+    url(r'^ajax/reportPopupDetails', 'get_report_popup_details', name="get_report_popup_details"),
+    url(r'^ajax/map/filter/', 'filter_map', name="filter_map"),
 )
 
 
-urlpatterns += patterns('django_fixmystreet.fixmystreet.views.api',
-    #next line to be deprecated...
-    url(_(r'^api/reports/$'), 'reports_pro',name='api_reports'),
-    url(_(r'^api/reports/mobile/$'), 'reports_pro_mobile',name='api_reports'),
-    url(_(r'^api/reports/map/$'), 'reports_pro',name='api_reports'),
-    url(_(r'^api/reports/citizen/nearest/$'), 'near_reports_citizen',name='api_reports'),
-    url(_(r'^api/reports/pro/nearest/$'), 'near_reports_pro',name='api_reports'),
-    url(_(r'^api/reports/citizen/$'), 'reports_citizen',name='api_reports'),
-    url(_(r'^api/reports/pro/$'), 'reports_pro',name='api_reports'),
+urlpatterns += patterns(
+    'django_fixmystreet.fixmystreet.views.api',
+    url(r'^api/reports/$', 'reports_pro', name='api_reports'),
+    url(r'^api/reports/mobile/$', 'reports_pro_mobile', name='api_reports'),
+    url(r'^api/reports/map/$', 'reports_pro', name='api_reports'),
+    url(r'^api/reports/citizen/nearest/$', 'near_reports_citizen', name='api_reports'),
+    url(r'^api/reports/pro/nearest/$', 'near_reports_pro', name='api_reports'),
+    url(r'^api/reports/citizen/$', 'reports_citizen', name='api_reports'),
+    url(r'^api/reports/pro/$', 'reports_pro', name='api_reports'),
     #url(_(r'^api/report/new/$'), 'create_report',name='api_report_new'),
-    url(_(r'^api/login/$'),'login_user',name='login_user'),
-    url(_(r'^api/logout/$'),'logout_user',name='logout_user'),
-    url(_(r'^api/load_categories/$'),'load_categories',name='load_categories'),
-    url(_(r'^api/load_zipcodes/$'),'load_zipcodes',name='load_zipcodes'),
-    url(_(r'^api/create-report/$'),Resource(ReportHandler), name="create_report_citizen"),
-    url(_(r'^api/create_report_photo/$'),'create_report_photo',name='create_report_photo'),
-    url(_(r'^api/commit-report/$'),'commit_report',name='commit_report'),
+    url(r'^api/login/$', 'login_user', name='login_user'),
+    url(r'^api/logout/$', 'logout_user', name='logout_user'),
+    url(r'^api/load_categories/$', 'load_categories', name='load_categories'),
+    url(r'^api/load_zipcodes/$', 'load_zipcodes', name='load_zipcodes'),
+    url(r'^api/create-report/$', Resource(ReportHandler), name="create_report_citizen"),
+    url(r'^api/create_report_photo/$', 'create_report_photo', name='create_report_photo'),
+    url(r'^api/commit-report/$', 'commit_report', name='commit_report'),
 )
-# urlpatterns += patterns('django_fixmystreet.fixmystreet.views.import',
-#     url(_(r'^import/report/close/$'), 'close_report',name='close_report'),
-#     url(_(r'^import/report/change_manager/$'), 'change_manager_report',name='change_manager_report'),
-# )
-
-# urlpatterns += patterns('django_fixmystreet.fixmystreet.views.export',
-#     url(_(r'^export/reports/entity/$'), 'entity_reports',name='entity_reports'),
-#     url(_(r'^export/reports/contractor/$'), 'contractor_reports',name='contractor_reports'),
-#     url(_(r'^export/reports/manager/$'), 'manager_reports',name='manager_reports'),
-# )
-
