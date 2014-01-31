577ac2fd0258beb342d1f301147e0de93a53797f
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2014-01-31 11:36:32 +0100

    popup send pdf email layout

--- django_fixmystreet/fixmystreet/static/js/common.js
+++ django_fixmystreet/fixmystreet/static/js/common.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
 
     if ($("#leftMenuPanePro .positionable").length) {
         //Update menu entries with coordinates
-        var USER_POSITION = new Object();
+        var USER_POSITION = {};
         if(navigator.geolocation){
             var options = {timeout:60000};
             var mapReference = this.map;
             navigator.geolocation.getCurrentPosition(
                 //success
                 function(position) {
-                    var latitude = POSY = position.coords.latitude;
-                    var longitude = POSX = position.coords.longitude;
+                    var latitude = position.coords.latitude;
+                    var longitude = position.coords.longitude;
                     var p = new Proj4js.Point(longitude, latitude);
                     Proj4js.transform(new Proj4js.Proj("EPSG:4326"), new Proj4js.Proj("EPSG:31370"), p);
                     USER_POSITION.x = p.x;
@@ -33,6 +33,27 @@ $(document).ready(function(){
             evt.preventDefault();
             evt.stopImmediatePropagation();
         }
+    });
+
+    $('.ajax-form').submit(function(evt) {
+        evt.preventDefault();
+        $form = $(this);
+        $form.removeClass('form-success form-error').addClass('loading');
+
+        $.ajax({
+            url:$form.prop("action"),
+            data: $form.serializeArray(),
+            type:"POST"
+        }).success(function () {
+            $form.addClass('form-success');
+            setTimeout(function () {
+                $form.removeClass('form-success');
+            }, 3000);
+        }).error(function () {
+            $form.addClass('form-error');
+        }).always(function () {
+            $form.removeClass('loading');
+        });
     });
 
     $("[data-toggle=popover]")
