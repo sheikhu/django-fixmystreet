76769a1b5afc020b9c03bd8cb8162c452cd06921
afuca <afuca@cirb.irisnet.be>
2014-02-13 16:06:25 +0100

    Fix ods data migration

--- django_fixmystreet/fixmystreet/migrations/0035_create_ods_views.py
+++ django_fixmystreet/fixmystreet/migrations/0035_create_ods_views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
 from south.db import db
-from south.v2 import SchemaMigration
+from south.v2 import DataMigration
 from django.db import models
 
+import os
+current_dir = os.path.dirname(__file__)
 
-class Migration(SchemaMigration):
+class Migration(DataMigration):
 
     def forwards(self, orm):
         db.execute_many(open("{0}/ods-views.sql".format(current_dir)).read())
