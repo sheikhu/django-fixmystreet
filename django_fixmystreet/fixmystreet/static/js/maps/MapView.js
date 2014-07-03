bac5061c4075795e96c7c1e3904633ecd65a0f56
Jonathan Sanchez Pando <jsanchezpando@cirb.irisnet.be>
2014-07-03 16:07:10 +0200

    change map center and zoom level FMS-254

--- django_fixmystreet/fixmystreet/static/js/maps/MapView.js
+++ django_fixmystreet/fixmystreet/static/js/maps/MapView.js
@@ -1,6 +1,6 @@
 var MapView = Backbone.View.extend({
     render: function () {
-        var map = L.map(this.el).setView([55.75, 37.58], 10);
+        var map = L.map(this.el).setView([50.84331852979277,4.357895514064874], 12);
 
         var nexrad = L.tileLayer.wms("http://geoserver.gis.irisnet.be/urbis/wms/gwc", {
             layers: 'urbisFR', // + LANGUAGE_ODE
