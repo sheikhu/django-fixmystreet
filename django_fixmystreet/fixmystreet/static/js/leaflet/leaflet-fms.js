05c84009d18ed9583a88bf9bf8ca12394145a736
Pascal Polleunus <ppo@especific.be>
2014-07-30 18:15:40 +0200

    Improvement.

--- django_fixmystreet/fixmystreet/static/js/leaflet/leaflet-fms.js
+++ django_fixmystreet/fixmystreet/static/js/leaflet/leaflet-fms.js
@@ -274,8 +274,8 @@ L.FixMyStreet.Map = L.Map.extend({
 
   toLatLng : function (latlng) {  // (L.LatLng or String or Object)
     if (latlng === undefined || latlng instanceof L.LatLng) { return latlng; }
-    if ('lat' in latlng && 'lng' in latlng) { return new L.LatLng(latlng.lat, latlng.lng); }
     if (typeof latlng === 'string') { return this.latLngFromString(latlng); }
+    if (typeof latlng === 'object' && 'lat' in latlng && 'lng' in latlng) { return new L.LatLng(latlng.lat, latlng.lng); }
     throw new Error('Invalid parameter. Expect L.LatLng or String ("0.123,-45.678") or Object ({lat: 0.123, lng: -45.678})');
   },
 
