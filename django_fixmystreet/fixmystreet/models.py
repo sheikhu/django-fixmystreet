a93492952ff156fe466a2a5d65ec035d17c906f0
David Franklin <dfranklin@cirb.irisnet.be>
2014-08-12 15:27:20 +0200

    FMS 338 :
    On essayait d'accéder à l'attribut d'un objet qui est n'avait encore aucune valeur :
    
    dans get_date_planned_available(self):
    self.accepted_at est null et on essayait d'accéder à self.accepted_at.year etc

--- django_fixmystreet/fixmystreet/models.py
+++ django_fixmystreet/fixmystreet/models.py
@@ -869,14 +869,15 @@ class Report(UserTrackedModel):
     def get_date_planned_available(self):
         dates = []
         start_date = datetime.date.today() + timedelta(days=1)
-        end_date = datetime.date(self.accepted_at.year, self.accepted_at.month, self.accepted_at.day) + timedelta(days=365)
+        if self.accepted_at:
+            end_date = datetime.date(self.accepted_at.year, self.accepted_at.month, self.accepted_at.day) + timedelta(days=365)
 
-        while (start_date < end_date):
-            dates.append(start_date)
+            while (start_date < end_date):
+                dates.append(start_date)
 
-            month = start_date.month
-            while (month == start_date.month):
-                start_date = start_date + timedelta(days=1)
+                month = start_date.month
+                while (month == start_date.month):
+                    start_date = start_date + timedelta(days=1)
 
         return dates
 
