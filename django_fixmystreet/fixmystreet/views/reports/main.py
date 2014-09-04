f2d042b47137fc01c8ac451a1a6c64af904764c5
David Franklin <dfranklin@cirb.irisnet.be>
2014-09-04 17:18:31 +0200

    FMS-338 Workaround set in place in order to prevent a citizen to mark an incident as done : - citizen can't see the button anymore - code setting an incident as "mark as done" has been commented too.

--- django_fixmystreet/fixmystreet/views/reports/main.py
+++ django_fixmystreet/fixmystreet/views/reports/main.py
@@ -171,16 +171,16 @@ def document(request, slug, report_id):
 def update(request, report_id):
     report = get_object_or_404(Report, id=report_id)
 
-    if 'is_fixed' in request.REQUEST:
-        #Update the status of report
-        report.status   = Report.SOLVED
-        report.fixed_at = datetime.datetime.now()
-        report.save()
-
-        if "pro" in request.path:
-            return HttpResponseRedirect(report.get_absolute_url_pro())
-        else:
-            return HttpResponseRedirect(report.get_absolute_url())
+    # if 'is_fixed' in request.REQUEST:
+    #     #Update the status of report
+    #     report.status   = Report.SOLVED
+    #     report.fixed_at = datetime.datetime.now()
+    #     report.save()
+    #
+    #     if "pro" in request.path:
+    #         return HttpResponseRedirect(report.get_absolute_url_pro())
+    #     else:
+    #         return HttpResponseRedirect(report.get_absolute_url())
 
     # ????? DEPRECATED ?????
     if request.method == 'POST':
