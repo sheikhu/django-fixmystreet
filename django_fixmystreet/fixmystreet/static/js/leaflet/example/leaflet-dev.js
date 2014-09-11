fbd633276f7c5e126777838ec442c72aed5cfb88
Pascal Polleunus <ppo@especific.be>
2014-09-11 12:18:30 +0200

    Add constant & function for dev.

--- django_fixmystreet/fixmystreet/static/js/leaflet/example/leaflet-dev.js
+++ django_fixmystreet/fixmystreet/static/js/leaflet/example/leaflet-dev.js
@@ -1,3 +1,5 @@
+var STATIC_URL = 'http://127.0.0.1:8000/static/';
+
 function getRandomLatLng(map) {
   var bounds = map.getBounds(),
       southWest = bounds.getSouthWest(),
@@ -6,4 +8,8 @@ function getRandomLatLng(map) {
       latSpan = northEast.lat - southWest.lat;
 
   return new L.LatLng(southWest.lat + latSpan * Math.random(), southWest.lng + lngSpan * Math.random());
+}
+
+function gettext(s) {
+  return s;
 }
