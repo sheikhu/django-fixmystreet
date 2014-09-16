4c9cac3db13de6fb89aedfc57c320a3a66b3428e
David Franklin <dfranklin@cirb.irisnet.be>
2014-09-16 09:32:15 +0200

    FMS 275 : code uncommented

--- django_fixmystreet/fixmystreet/static/js/report_refused.js
+++ django_fixmystreet/fixmystreet/static/js/report_refused.js
@@ -1,20 +1,20 @@
-//$(document).ready(function() {
-//
-//     $("select[name='sel_refuse']").change(sel_refuse_handler);
-//});
-//
-//function sel_refuse_handler(){
-//
-//    var textarea = $('#divRefuse textarea');
-//    var select_value = $("select[name='sel_refuse']").val();
-//
-//    if(select_value != 'opt_refuse_0') {
-//        textarea.val(sel_refuse_content[select_value]);
-//    }
-//
-//}
-//
-//var sel_refuse_content = {};
-//sel_refuse_content['opt_refuse_1'] = "This is a content";
-//sel_refuse_content['opt_refuse_2'] = "This is another content";
-//sel_refuse_content['opt_refuse_3'] = "This is a third content \n\twith carriage return and a tab";
+$(document).ready(function() {
+
+     $("select[name='sel_refuse']").change(sel_refuse_handler);
+});
+
+function sel_refuse_handler(){
+
+    var textarea = $('#divRefuse textarea');
+    var select_value = $("select[name='sel_refuse']").val();
+
+    if(select_value != 'opt_refuse_0') {
+        textarea.val(sel_refuse_content[select_value]);
+    }
+
+}
+
+var sel_refuse_content = {};
+sel_refuse_content['opt_refuse_1'] = "This is a content";
+sel_refuse_content['opt_refuse_2'] = "This is another content";
+sel_refuse_content['opt_refuse_3'] = "This is a third content \n\twith carriage return and a tab";
