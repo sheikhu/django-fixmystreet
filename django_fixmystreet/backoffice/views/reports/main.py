24a3a51e0d3ad9e4522d0b456d852c9cc4b92d3b
David Franklin <dfranklin@cirb.irisnet.be>
2014-09-22 09:54:17 +0200

    FMS-250 : small fix (could not ask to reopen a pro incident even when logged as a pro)

--- django_fixmystreet/backoffice/views/reports/main.py
+++ django_fixmystreet/backoffice/views/reports/main.py
@@ -294,7 +294,7 @@ def merge(request, slug, report_id):
     }, context_instance=RequestContext(request))
 
 def reopen_request(request, slug, report_id):
-    report = get_object_or_404(Report, id=report_id, private=False)
+    report = get_object_or_404(Report, id=report_id, private=True)
 
     if request.method == "POST":
         comment = None
