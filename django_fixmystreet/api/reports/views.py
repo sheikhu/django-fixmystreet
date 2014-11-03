ff592c628b3e524c9fb12ca68f52fab8473824ce
Pascal Polleunus <ppo@especific.be>
2014-11-03 13:46:04 +0100

    Add exception handling. Improve code readability.

--- django_fixmystreet/api/reports/views.py
+++ django_fixmystreet/api/reports/views.py
@@ -4,57 +4,65 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from django_fixmystreet.fmsproxy.models import AssignmentHandler
+from django_fixmystreet.fmsproxy.models import AssignmentHandler, \
+    NotWaitingForThirdPartyError, NotAssignedToThirdPartyError, NotAuthorizedToThirdPartyError
 from . import serializers
 from ..views import FmsPrivateViewMixin
 
 
 class AbstractReportAssignmentView(FmsPrivateViewMixin, APIView):
 
-    def _get_bad_request(self, serializer):
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def _get_assignment_handler(self, pk, user):
+        try:
+            return AssignmentHandler(pk, user)
+        except (NotWaitingForThirdPartyError, NotAssignedToThirdPartyError, NotAuthorizedToThirdPartyError):
+            return self._get_response_403()
+
+    def _get_response_200(self, context=None):
+        if not context:
+            context = {"message": "Request successfully processed."}
+        return Response(context)
+
+    def _get_response_400(self, errors):
+        return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def _get_response_403(self, context=None):
+        if not context:
+            context = {"detail": "Access or action forbidden."}
+        return Response(context, status=status.HTTP_403_FORBIDDEN)
 
 
 class ReportAssignmentAcceptView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
         serializer = serializers.ReportAssignmentAcceptSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
-        if serializer.is_valid():
-            assignment = AssignmentHandler(pk, request.user)
-            assignment.accept(serializer.data)
-            response_data = {
-                "message": "Request successfully processed.",
-            }
-            return Response(response_data)
+        if not serializer.is_valid():
+            return self._get_response_400(serializer.errors)
 
-        return self._get_bad_request(serializer)
+        assignment = self._get_assignment(pk, request.user)
+        assignment.accept(serializer.data)
+        return self._get_response_200()
 
 
 class ReportAssignmentRejectView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
         serializer = serializers.ReportAssignmentRejectSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
-        if serializer.is_valid():
-            assignment = AssignmentHandler(pk, request.user)
-            assignment.reject(serializer.data)
-            response_data = {
-                "message": "Request successfully processed.",
-            }
-            return Response(response_data)
+        if not serializer.is_valid():
+            return self._get_response_400(serializer.errors)
 
-        return self._get_bad_request(serializer)
+        assignment = self._get_assignment(pk, request.user)
+        assignment.reject(serializer.data)
+        return self._get_response_200()
 
 
 class ReportAssignmentCloseView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
         serializer = serializers.ReportAssignmentCloseSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
-        if serializer.is_valid():
-            assignment = AssignmentHandler(pk, request.user)
-            assignment.close(serializer.data)
-            response_data = {
-                "message": "Request successfully processed.",
-            }
-            return Response(response_data)
+        if not serializer.is_valid():
+            return self._get_response_400(serializer.errors)
 
-        return self._get_bad_request(serializer)
+        assignment = self._get_assignment(pk, request.user)
+        assignment.close(serializer.data)
+        return self._get_response_200()
