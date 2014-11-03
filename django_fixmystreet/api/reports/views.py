4ba4210122278a89f83387d41bfcbdca02213db9
Pascal Polleunus <ppo@especific.be>
2014-11-03 13:55:12 +0100

    Minor changes & fixes.

--- django_fixmystreet/api/reports/views.py
+++ django_fixmystreet/api/reports/views.py
@@ -12,9 +12,9 @@ from ..views import FmsPrivateViewMixin
 
 class AbstractReportAssignmentView(FmsPrivateViewMixin, APIView):
 
-    def _get_assignment_handler(self, pk, user):
+    def _get_assignment_handler(self, report_id, user):
         try:
-            return AssignmentHandler(pk, user)
+            return AssignmentHandler(report_id, user)
         except (NotWaitingForThirdPartyError, NotAssignedToThirdPartyError, NotAuthorizedToThirdPartyError):
             return self._get_response_403()
 
@@ -35,11 +35,11 @@ class AbstractReportAssignmentView(FmsPrivateViewMixin, APIView):
 class ReportAssignmentAcceptView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
-        serializer = serializers.ReportAssignmentAcceptSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
+        serializer = serializers.ReportAssignmentAcceptSerializer(data=request.DATA)  # pylint: disable=E1120,E1123
         if not serializer.is_valid():
             return self._get_response_400(serializer.errors)
 
-        assignment = self._get_assignment(pk, request.user)
+        assignment = self._get_assignment_handler(pk, request.user)
         assignment.accept(serializer.data)
         return self._get_response_200()
 
@@ -47,11 +47,11 @@ class ReportAssignmentAcceptView(AbstractReportAssignmentView):
 class ReportAssignmentRejectView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
-        serializer = serializers.ReportAssignmentRejectSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
+        serializer = serializers.ReportAssignmentRejectSerializer(data=request.DATA)  # pylint: disable=E1120,E1123
         if not serializer.is_valid():
             return self._get_response_400(serializer.errors)
 
-        assignment = self._get_assignment(pk, request.user)
+        assignment = self._get_assignment_handler(pk, request.user)
         assignment.reject(serializer.data)
         return self._get_response_200()
 
@@ -59,10 +59,10 @@ class ReportAssignmentRejectView(AbstractReportAssignmentView):
 class ReportAssignmentCloseView(AbstractReportAssignmentView):
 
     def post(self, request, pk=None, format=None):
-        serializer = serializers.ReportAssignmentCloseSerializer(data=request.DATA)  # pylint: disable=C0103,E1120,E1123
+        serializer = serializers.ReportAssignmentCloseSerializer(data=request.DATA)  # pylint: disable=E1120,E1123
         if not serializer.is_valid():
             return self._get_response_400(serializer.errors)
 
-        assignment = self._get_assignment(pk, request.user)
+        assignment = self._get_assignment_handler(pk, request.user)
         assignment.close(serializer.data)
         return self._get_response_200()
