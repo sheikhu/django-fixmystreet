8cb4e58a26ccb2d132197eb15a939a6c65757a83
Pascal Polleunus <ppo@especific.be>
2014-11-14 15:01:14 +0100

    Conversion JSON to Python is now done automatically (via DRF Parser).

--- django_fixmystreet/api/hooks/views.py
+++ django_fixmystreet/api/hooks/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103,W0613,W0622
 from ..views import PrivateApiView
-from ..utils import dict_walk_json_to_python, to_upper_camel_case
+from ..utils.conversion import to_upper_camel_case
 from ...webhooks import inbound as inbound_webhooks
 
 
@@ -11,9 +11,8 @@ class UnknownWebhookError(LookupError): pass  # pylint: disable=C0321
 class HooksView(PrivateApiView):
 
     def post(self, request, format=None):
-        request_data = dict_walk_json_to_python(request.DATA)  # @TODO: Do that earlier in DRF process (cf. parsers).
-        meta = request_data["meta"]
-        data = request_data["data"]
+        meta = request.DATA["meta"]
+        data = request.DATA["data"]
 
         # Build handler class name.
         class_name = "{}{}{}InWebhook".format(
