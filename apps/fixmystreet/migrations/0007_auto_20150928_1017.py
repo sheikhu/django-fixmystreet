4e6a6798de065e78ded6c9cbda8642b92031dd20
afuca <afuca@cirb.irisnet.be>
2015-09-28 14:06:24 +0200

    FMS-533: Migration script for entity and existant reports

---
+++ apps/fixmystreet/migrations/0007_auto_20150928_1017.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+from apps.fixmystreet.models import OrganisationEntity as OrganisationEntityModel
+from apps.fixmystreet.models import Report
+
+
+
+def migrate_osiris_entity(apps, schema_editor):
+    OrganisationEntity = apps.get_model("fixmystreet", "OrganisationEntity")
+    ReportCategory     = apps.get_model("fixmystreet", "ReportCategory")
+
+    # Change type of osiris
+    osiris = OrganisationEntity.objects.get(name_fr__iexact="osiris")
+    osiris.type = OrganisationEntityModel.REGION
+    osiris.save()
+
+    # Fix dispatching of osiris group
+    osiris_group = osiris.associates.all()[0]
+
+    for category in ReportCategory.objects.all():
+        osiris_group.dispatch_categories.add(category)
+
+
+def migrate_reports(apps, schema_editor):
+    osiris = OrganisationEntityModel.objects.get(name_fr__iexact="osiris")
+    osiris_group = osiris.associates.all()[0]
+
+    reports_osiris = Report.objects.filter(contractor=osiris)
+
+    for report in reports_osiris:
+        report.contractor = None
+        report.responsible_entity = osiris
+        report.responsible_department = osiris_group
+        report.status = Report.MANAGER_ASSIGNED
+        report.save()
+
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fixmystreet', '0006_faqpage'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_osiris_entity),
+        migrations.RunPython(migrate_reports),
+    ]
