bdfaba5746804ae3b9577cf66372b9dde73f0f00
Sebastien Lafont <slafont@cirb.brussels>
2015-12-21 11:55:46 +0100

    FMS-661 Redirection to 2nd step when sending an incident with a not logged in pro email

--- apps/fixmystreet/views/reports/main.py
+++ apps/fixmystreet/views/reports/main.py
@@ -26,6 +26,7 @@ logger = logging.getLogger(__name__)
 
 def new(request):
     ReportFileFormSet = inlineformset_factory(Report, ReportFile, form=ReportFileForm, extra=0)
+    citizen_error = False
 
     pnt = dict_to_point(request.REQUEST)
     report = None
@@ -84,6 +85,8 @@ def new(request):
             else:
                 report = None
 
+        citizen_error = not citizen_form.is_valid()
+
     else:
         report_form = CitizenReportForm(initial={
             'x': request.REQUEST.get('x'),
@@ -103,7 +106,8 @@ def new(request):
         "file_formset": file_formset,
         "report_form": report_form,
         "citizen_form": citizen_form,
-        "pnt": pnt
+        "pnt": pnt,
+        "citizen_error" : citizen_error
     }, context_instance=RequestContext(request))
 
 
