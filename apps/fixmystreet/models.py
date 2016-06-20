e429494dcd8d8a6520f5a2bdb7423988edf64d81
David Franklin <dfranklin@cirb.irisnet.be>
2016-06-20 11:35:35 +0200

    FMS-743 : bug fix for exif

--- apps/fixmystreet/models.py
+++ apps/fixmystreet/models.py
@@ -1396,14 +1396,17 @@ class ReportFile(ReportAttachment):
             e = image._getexif()       # returns None if no EXIF data
             if e is not None:
                 exif = dict(e.items())
-                orientation = exif[orientation]
+                try:
+                    orientation = exif[orientation]
 
-                if orientation == 3:
-                    image = image.transpose(Image.ROTATE_180)
-                elif orientation == 6:
-                    image = image.transpose(Image.ROTATE_270)
-                elif orientation == 8:
-                    image = image.transpose(Image.ROTATE_90)
+                    if orientation == 3:
+                        image = image.transpose(Image.ROTATE_180)
+                    elif orientation == 6:
+                        image = image.transpose(Image.ROTATE_270)
+                    elif orientation == 8:
+                        image = image.transpose(Image.ROTATE_90)
+                except KeyError:
+                    logger.info("exif orientation not found. Report id : %s " % self.report.id)
 
         size = image.size
 
