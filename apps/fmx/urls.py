a3a94e84b17cba8408380f842126b13bbb623561
Sebastien Lafont <slafont@cirb.brussels>
2017-01-18 17:11:45 +0100

    FMS : Get duplicates from coordinates

--- apps/fmx/urls.py
+++ apps/fmx/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('apps.fmx.views',
     (r'^ack/$', 'ack'),
     (r'^categories/$', 'categories'),
     (r'^stats/$', 'stats'),
+    (r'^duplicates/$', 'duplicates'),
 
     (r'^(?P<report_id>\d+)$', 'detail'),
     (r'^(?P<report_id>\d+)/attachments/$', 'attachments'),
--- apps/fmx/views.py
+++ apps/fmx/views.py
@@ -55,38 +55,44 @@ def categories(request):
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 
-def detail(request, report_id):
+def generate_report_response(report):
     response = get_response()
 
+    responsible = "%s - %s (%s)" %(report.responsible_department.name,report.responsible_entity.name, report.responsible_department.phone)
+
+    response['response'] = {
+        "id": report.get_ticket_number(),
+        "status": report.status,
+        "statusLabel": report.get_public_status_display(),
+        "category": report.display_category(),
+        "created": report.created.strftime('%d/%m/%Y'),
+        "responsible": responsible,
+        "address": report.display_address(),
+        "point": {
+            "x": report.point.x,
+            "y": report.point.y
+        },
+    }
+
+    # Generate PDF absolute url
+    site = Site.objects.get_current()
+    base_url = "http://{}".format(site.domain.rstrip("/"))
+    relative_pdf_url = reverse("report_pdf", args=[report.id]).lstrip("/")
+    absolute_pdf_url = "{}/{}".format(base_url, relative_pdf_url)
+
+    response['_links'] = {
+        "self" : "/%s" % report.id,
+        "download" : absolute_pdf_url
+    }
+
+    return response
+
+def detail(request, report_id):
     try:
         report = Report.objects.all().public().get(id=report_id)
+        response = generate_report_response(report)
 
-        responsible = "%s - %s (%s)" %(report.responsible_department.name,report.responsible_entity.name, report.responsible_department.phone)
-
-        response['response'] = {
-            "id": report.get_ticket_number(),
-            "status": report.status,
-            "statusLabel": report.get_public_status_display(),
-            "category": report.display_category(),
-            "created": report.created.strftime('%d/%m/%Y'),
-            "responsible": responsible,
-            "address": report.display_address(),
-            "point": {
-                "x": report.point.x,
-                "y": report.point.y
-            },
-        }
-
-        # Generate PDF absolute url
-        site = Site.objects.get_current()
-        base_url = "http://{}".format(site.domain.rstrip("/"))
-        relative_pdf_url = reverse("report_pdf", args=[report_id]).lstrip("/")
-        absolute_pdf_url = "{}/{}".format(base_url, relative_pdf_url)
-
-        response['_links'] = {
-            "self" : "/%s" % report.id,
-            "download" : absolute_pdf_url
-        }
+        return return_response(response)
     except Report.DoesNotExist:
         exception = {
           "type": "ERROR",
@@ -95,8 +101,6 @@ def detail(request, report_id):
         }
 
         return return_exception(exception, 404)
-
-    return return_response(response)
 
 from apps.fixmystreet.stats import ReportCountQuery
 from apps.fixmystreet.views.main import DEFAULT_SQL_INTERVAL_CITIZEN
@@ -110,5 +114,45 @@ def stats(request):
         "inProgressCount": report_counts.recent_updated(),
         "closedCount": report_counts.recent_fixed()
     }
+
+    return return_response(response)
+
+from django.contrib.gis.geos import fromstr
+def duplicates(request):
+
+    x = request.GET.get("x", None)
+    y = request.GET.get("y", None)
+
+    if x is None or y is None:
+        exception = {
+          "type": "ERROR",
+          "code": 404,
+          "description": "Missing coordinates"
+        }
+
+        return return_exception(exception, 404)
+
+    try:
+        #Check if coordinates are float
+        float(x)
+        float(y)
+    except ValueError as e:
+        exception = {
+          "type": "ERROR",
+          "code": 404,
+          "description": "Invalid coordinates"
+        }
+
+        return return_exception(exception, 404)
+
+    pnt = fromstr("POINT(" + x + " " + y + ")", srid=31370)
+    reports_nearby = Report.objects.all().visible().public().near(pnt, 20).related_fields()[0:6]
+
+    response = get_response()
+    response["response"] = []
+    for report in reports_nearby:
+        res = generate_report_response(report)
+        if res.get("response", None) is not None:
+            response["response"].append(res.get("response", None))
 
     return return_response(response)
