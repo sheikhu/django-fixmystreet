719b2bcbfdc753dc707c07ea7c8d1c28959dce54
afuca <afuca@cirb.brussels>
2017-05-15 09:13:12 +0200

    FMX-1087: Fix mapping command.

--- apps/fixmystreet/management/commands/generate_new_categories_tree.py
+++ apps/fixmystreet/management/commands/generate_new_categories_tree.py
@@ -157,7 +157,8 @@ class Command(BaseCommand):
                     "fms_id": int(data[NEW_LVL_2_ID_IDX]),
                     "abp_id": int(data[ABP_TYPE_ID_IDX])
                 },
-                "model": "abp.type"
+                "model": "abp.type",
+                "pk": int(data[NEW_LVL_2_ID_IDX])
             }
             self.FIXTURES_FMSPROXY[int(data[NEW_LVL_2_ID_IDX])] = fmsproxy_type
 
@@ -176,18 +177,34 @@ class Command(BaseCommand):
                     "fms_id": fms_id,
                     "abp_id": nature_id
                 },
-                "model": "abp.nature"
+                "model": "abp.nature",
+                "pk": fms_id
             }
             self.FIXTURES_FMSPROXY[fms_id] = fmsproxy_nature
 
+            # FMSProxy subnature
             try:
-                # FMSProxy bagtype
+                fmsproxy_subnature = {
+                    "fields": {
+                        "fms_id": int(data[NEW_LVL_4_ID_IDX]),
+                        "abp_id": int(data[ABP_SUBNATURE_ID_IDX])
+                    },
+                    "model": "abp.subnature",
+                    "pk": int(data[NEW_LVL_4_ID_IDX])
+                }
+                self.FIXTURES_FMSPROXY[int(data[NEW_LVL_3_ID_IDX])] = fmsproxy_subnature
+            except ValueError:
+                pass
+
+            # FMSProxy bagtype
+            try:
                 fmsproxy_bagtype = {
                     "fields": {
                         "fms_id": int(data[NEW_LVL_4_ID_IDX]),
                         "abp_id": int(data[ABP_BAGTYPE_ID_IDX])
                     },
-                    "model": "abp.bagtype"
+                    "model": "abp.bagtype",
+                    "pk": int(data[NEW_LVL_4_ID_IDX])
                 }
                 self.FIXTURES_FMSPROXY[int(data[NEW_LVL_4_ID_IDX])] = fmsproxy_bagtype
             except ValueError:
