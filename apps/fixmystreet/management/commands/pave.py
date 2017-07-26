ba97b91c327e840a95d1216888e1bb95988264e3
afuca <afuca@cirb.brussels>
2017-07-26 11:24:28 +0200

    FMX-1221: PAVE - set address

--- apps/fixmystreet/management/commands/pave.py
+++ apps/fixmystreet/management/commands/pave.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.gis.geos import GEOSGeometry
 
 from optparse import make_option
 
 from apps.fixmystreet.models import Report, FMSUser
 
-import csv, logging, datetime
+import csv, json, logging, requests
 
 logger = logging.getLogger("fixmystreet")
 
@@ -46,6 +47,9 @@ class Command(BaseCommand):
 
     PAVE_CATEGORY_IDX = 1
 
+    PAVE_LAT_IDX = 3
+    PAVE_LONG_IDX = 4
+
     def handle(self, *args, **options):
         pave_csv = options['pave_csv'] if options['pave_csv'] else 'apps/resources/PAVE/pave.csv'
 
@@ -66,13 +70,13 @@ class Command(BaseCommand):
                     citizen=self.get_pave_user()
                 )
 
-                try:
-                    self.set_category(report, row)
-                except KeyError:
-                    errors.append(row)
-                    continue
+                # try:
+                #     self.set_category(report, row)
+                # except KeyError:
+                #     errors.append(row)
+                #     continue
 
-                # self.set_address(report, row)
+                self.set_address(report, row)
                 # self.dispatch(report, row)
                 # self.set_description(report, row)
                 # self.set_attachments(report, row)
@@ -103,3 +107,53 @@ class Command(BaseCommand):
 
         report.category = pave_category['category']
         report.secondary_category = pave_category['secondary_category']
+
+
+    def set_address(self, report, row):
+        # Set point
+        x = row[self.PAVE_LAT_IDX].replace(',','.')
+        y = row[self.PAVE_LONG_IDX].replace(',','.')
+
+        report.point = GEOSGeometry(
+            "POINT(" + x + " " + y + ")",
+            srid=4326
+        )
+
+        # Fetch address from urbis
+        json_data = {
+            "point": {
+                "x": x,
+                "y": y
+            },
+            "SRS_In":"4326"
+        }
+        json_data_fr = json_data
+        json_data_fr["language"] = "fr"
+
+        json_data_nl = json_data
+        json_data_nl["language"] = "nl"
+
+        address_fr = self._get_urbis_address(json_data_fr)
+        address_nl = self._get_urbis_address(json_data_nl)
+
+        # Set address
+        if address_fr:
+            report.address_fr = address_fr.get('address').get('street').get('name')
+            report.address_number = address_fr.get('address').get('number')
+            report.postalcode = address_fr.get('address').get('street').get('postCode')
+            # report.address_regional
+
+        if address_nl:
+            report.address_nl = address_fr.get('address').get('street').get('name')
+            report.address_number = address_fr.get('address').get('number')
+            report.postalcode = address_fr.get('address').get('street').get('postCode')
+            # report.address_regional
+
+    def _get_urbis_address(self, json_data):
+        response = requests.get("http://geoservices.irisnet.be/localization/Rest/Localize/getaddressfromxy?json=" + json.dumps(json_data))
+
+        if response.status_code == requests.codes.ok:
+            return response.json().get('result')
+
+        logger.error('Address: cannot fetch urbis address')
+        return None
